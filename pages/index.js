b6ed87461f58fed77f9229aa69813b137ad483aa
noony1992 <67756931+noony1992@users.noreply.github.com>
2023-07-17 21:58:30 +0100

    fixed localhost redirect issue

--- pages/index.js
+++ pages/index.js
@@ -119,7 +119,7 @@ export default function Home() {
                       className="z-999 hover:bg-gray-200 rounded p-0.5"
                       onClick={(e) => {
                         e.stopPropagation();
-                        Router.push(`http://localhost:3000/profile/${thread.user.username}`);
+                        Router.push(`/profile/${thread.user.username}`);
                       }}
                     >
                       <h5 className="text-sm italic text-gray-500 inline-flex">
